14746420bf7f1eb34189d579f9279705103e8ba5
Browarnik123 <yay8001@gmail.com>
2024-11-03 18:30:21 +0100

    naprawa niedziałającego wyjścia z menu #3
    naprawa niedziałającego wyjścia z menu

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -1,16 +1,8 @@
-<<<<<<< Updated upstream
-import { TouchableOpacity, View, Modal, Text, Dimensions, ScrollView, Animated } from 'react-native';
-=======
 import { TouchableOpacity, View, Text, Dimensions, Animated } from 'react-native';
->>>>>>> Stashed changes
 import { useState, useEffect } from 'react';
 import { LanguageSwitch } from '~/components/LanguageSwitch';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { useTranslation } from 'react-i18next';
-<<<<<<< Updated upstream
-import { SafeAreaView } from 'react-native';
-=======
->>>>>>> Stashed changes
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -22,52 +14,33 @@ interface SlidingMenuProps {
 export function SlidingMenu({ visible, onClose }: SlidingMenuProps) {
   const [slideAnim] = useState(new Animated.Value(-screenWidth));
   const { t } = useTranslation();
-<<<<<<< Updated upstream
-=======
   const [isMenuMounted, setIsMenuMounted] = useState(false);
->>>>>>> Stashed changes
 
   const menuOptions = [
     {
       label: t('menu.map'),
-<<<<<<< Updated upstream
-      icon: <FontAwesome5 name="map" size={20} color="gray" />,
-=======
       icon: <FontAwesome5 name="map" size={20} color="black" />,
->>>>>>> Stashed changes
       onPress: () => {
         console.log('Open Map');
       }
     },
     {
       label: t('menu.objects'),
-<<<<<<< Updated upstream
-      icon: <FontAwesome5 name="box" size={20} color="gray" />,
-=======
       icon: <FontAwesome5 name="box" size={20} color="black" />,
->>>>>>> Stashed changes
       onPress: () => {
         console.log('Open Objects');
       }
     },
     {
       label: t('menu.profile'),
-<<<<<<< Updated upstream
-      icon: <FontAwesome5 name="user" size={20} color="gray" />,
-=======
       icon: <FontAwesome5 name="user" size={20} color="black" />,
->>>>>>> Stashed changes
       onPress: () => {
         console.log('Open Profile');
       }
     },
     {
       label: t('menu.settings'),
-<<<<<<< Updated upstream
-      icon: <FontAwesome5 name="cog" size={20} color="gray" />,
-=======
       icon: <FontAwesome5 name="cog" size={20} color="black" />,
->>>>>>> Stashed changes
       onPress: () => {
         console.log('Open Settings');
       }
@@ -76,24 +49,15 @@ export function SlidingMenu({ visible, onClose }: SlidingMenuProps) {
 
   const logoutOption = {
     label: t('menu.logout'),
-<<<<<<< Updated upstream
-    icon: <FontAwesome5 name="sign-out-alt" size={20} color="gray" />,
-=======
     icon: <FontAwesome5 name="sign-out-alt" size={20} color="black" />,
->>>>>>> Stashed changes
     onPress: () => {
       console.log('Log out');
     }
   };
 
   useEffect(() => {
-<<<<<<< Updated upstream
-    console.log("SlidingMenu visibility:", visible);
-    if (visible) {
-=======
     if (visible) {
       setIsMenuMounted(true);
->>>>>>> Stashed changes
       Animated.timing(slideAnim, {
         toValue: 0,
         duration: 300,
@@ -104,72 +68,6 @@ export function SlidingMenu({ visible, onClose }: SlidingMenuProps) {
         toValue: -screenWidth,
         duration: 300,
         useNativeDriver: true,
-<<<<<<< Updated upstream
-      }).start();
-    }
-  }, [visible]);
-
-  return (
-    <Modal
-      visible={visible}
-      transparent={true}
-      onRequestClose={onClose}
-      presentationStyle="overFullScreen"
-    >
-      <SafeAreaView style={{ 
-        flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        position: 'absolute',
-        top: 60,
-        bottom: 0,
-        left: 0,
-        right: 0
-      }}>
-        <TouchableOpacity onPress={onClose} style={{ flex: 1 }}>
-          <Text>Close Menu</Text>
-        </TouchableOpacity>
-        <Animated.View
-          style={{
-            backgroundColor: 'white',
-            width: '60%',
-            height: '100%',
-            paddingHorizontal: 10,
-            transform: [{ translateX: slideAnim }],
-          }}
-        >
-          <View className="flex-1">
-            {/* Adding marginTop here to push menu options downward */}
-            <View className="space-y-4 mb-6" style={{ marginTop: 120 }}>
-              {menuOptions.map((option, index) => (
-                <TouchableOpacity
-                  key={index}
-                  onPress={option.onPress}
-                  className="bg-gray-100 p-3 rounded-lg flex-row items-center"
-                >
-                  {option.icon}
-                  <Text className="text-lg ml-3">{option.label}</Text>
-                </TouchableOpacity>
-              ))}
-            </View>
-            <View className="flex-1 justify-between">
-              <View className="mt-8">
-                <TouchableOpacity
-                  onPress={logoutOption.onPress}
-                  className="bg-gray-100 p-3 rounded-lg flex-row items-center"
-                >
-                  {logoutOption.icon}
-                  <Text className="text-lg ml-3">{logoutOption.label}</Text>
-                </TouchableOpacity>
-              </View>
-              <View className="pb-6">
-                <LanguageSwitch />
-              </View>
-            </View>
-          </View>
-        </Animated.View>
-      </SafeAreaView>
-    </Modal>
-=======
       }).start(() => {
         setIsMenuMounted(false);
       });
@@ -268,6 +166,5 @@ export function SlidingMenu({ visible, onClose }: SlidingMenuProps) {
         </View>
       </Animated.View>
     </>
->>>>>>> Stashed changes
   );
 }
