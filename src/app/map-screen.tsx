e82aa8465d84f7e2fa5eba0b7495eb79f20fefe1
Kamil Kula <s205833@sggw.edu.pl>
2024-11-05 19:59:36 +0100

    apply tailwind styles

--- src/app/map-screen.tsx
+++ src/app/map-screen.tsx
@@ -1,13 +1,11 @@
 import React, { useRef, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapLibreGL from '@maplibre/maplibre-react-native';
-
-// Import svg icons
 import LightGreenDot from '../../assets/ellipse1.svg';
 import DarkGreenDot from '../../assets/ellipse2.svg';
 import MenuIcon from '../../assets/menus1.svg';
 import NavigationIcon from '../../assets/navigation.svg';
-import SearchIcon1 from '../../assets/search1.svg';;
+import SearchIcon1 from '../../assets/search1.svg';
 import SearchIcon2 from '../../assets/search2.svg';
 
 MapLibreGL.setAccessToken(null);
@@ -22,26 +20,25 @@ const campusCenter = [21.04635389581634, 52.16357007158958];
 
 export default function MapExample() {
   const camera = useRef(null);
-  const [isExpanded, setIsExpanded] = useState(false); // Track if the search bar is expanded
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleSearchBar = () => {
     setIsExpanded(!isExpanded);
   };
 
   const handleMapPress = () => {
-    setIsExpanded(true); // Expand search bar when user taps on the map
+    setIsExpanded(true);
   };
 
   return (
-    <View style={styles.container}>
+    <View className="flex-1">
       {/* Navbar */}
-      <View style={styles.navbar}>
-        <TouchableOpacity style={styles.menuButton}>
+      <View className="absolute left-0 right-0 top-0 z-20 h-20 flex-row items-center justify-between bg-[#0F4530] px-4">
+        <TouchableOpacity className="p-2">
           <MenuIcon width={40} height={40} fill="#FFF" />
         </TouchableOpacity>
-        <Text style={styles.logoText}>logo/nazwa</Text>
-        <TouchableOpacity style={styles.searchButton} onPress={toggleSearchBar}>
-          {/* Toggle search/navigation icon */}
+        <Text className="text-lg font-bold text-white">logo/nazwa</Text>
+        <TouchableOpacity className="p-2" onPress={toggleSearchBar}>
           {isExpanded ? (
             <NavigationIcon width={40} height={40} fill="#FFF" />
           ) : (
@@ -51,35 +48,38 @@ export default function MapExample() {
       </View>
 
       {/* Search Bar */}
-      <View style={[styles.searchContainer, { height: isExpanded ? 130 : 70 }]}>
+      <View
+        className={`absolute left-4 right-4 top-16 z-10 mt-9 rounded-full bg-white p-3 h-20 ${
+          isExpanded ? 'h-32' : 'h-15'
+        }`}>
         {isExpanded ? (
           <>
-            <View style={styles.searchRow}>
-              <LightGreenDot width={20} height={20} style={styles.dot} />
+            <View className="mb-2 flex-row items-center ml-4">
+              <LightGreenDot width={20} height={20} className="ml-4 mr-2" />
               <TextInput
-                style={styles.searchInput}
+                className="ml-2 mt-1 flex-1 rounded-md bg-white px-4 text-lg"
                 placeholder="Your location"
-                placeholderTextColor="#000000"
+                placeholderTextColor="#000"
               />
             </View>
-            <View style={styles.separator} />
-            <View style={styles.searchRow}>
-              <DarkGreenDot width={20} height={20} style={styles.dot} />
+            <View className="my-1 ml-16 h-px w-5/6 bg-black" />
+            <View className="flex-row items-center ml-4">
+              <DarkGreenDot width={20} height={20} className="ml-4 mr-2" />
               <TextInput
-                style={styles.searchInput}
+                className="ml-2 mt-1 flex-1 rounded-md bg-white px-4 text-lg"
                 placeholder="Destination"
-                placeholderTextColor="#000000"
+                placeholderTextColor="#000"
               />
             </View>
           </>
         ) : (
-          <View style={styles.searchRow}>
-          <SearchIcon1 width={28} height={28} style={styles.searchIcon} />
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Search"
-            placeholderTextColor="#000000"
-          />
+          <View className="flex-row items-center flex-1">
+            <SearchIcon1 width={28} height={28} className="mr-2" />
+            <TextInput
+              className="ml-3 flex-1 text-lg ml-8"
+              placeholder="Search"
+              placeholderTextColor="#000"
+            />
           </View>
         )}
       </View>
@@ -89,8 +89,7 @@ export default function MapExample() {
         style={{ flex: 1 }}
         logoEnabled={false}
         styleURL="https://americanamap.org/style.json"
-        onPress={handleMapPress} // Detect tap on map
-      >
+        onPress={handleMapPress}>
         <MapLibreGL.Camera
           ref={camera}
           centerCoordinate={campusCenter}
@@ -103,80 +102,3 @@ export default function MapExample() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  navbar: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 85,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 15,
-    backgroundColor: '#0F4530',
-    zIndex: 2,
-  },
-  menuButton: {
-    padding: 10,
-  },
-  logoText: {
-    fontSize: 18,
-    color: '#FFF',
-    fontFamily: 'inter'
-  },
-  searchButton: {
-    padding: 10,
-  },
-  searchContainer: {
-    position: 'absolute',
-    top: 70,
-    left: 15,
-    right: 15,
-    zIndex: 1,
-    backgroundColor: '#FFF',
-    borderRadius: 40,
-    padding: 10,
-    overflow: 'hidden',
-    marginTop: 35
-  },
-  searchRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  dot: {
-    marginLeft: 15,
-    marginTop: 3,
-  },
-  searchInput: {
-    backgroundColor: '#FFF',
-    borderRadius: 20,
-    paddingHorizontal: 15,
-    top: 7,
-    left: 13,
-    fontSize: 19,
-    fontFamily: 'inter',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    marginTop: 5,
-  },
-  separator: {
-    color: '#000000',
-    height: 1,
-    backgroundColor: '#000000',
-    width: '83%',
-    marginLeft: 50,
-    marginVertical: 5,
-  },
-  searchIcon: {
-    marginRight: 10,
-    top: 10,
-    left: 12
-  },
-});
