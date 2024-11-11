2656580473571cacd5ef9ff5ee5c3fca9e3566a8
Kamil Kula <s205833@sggw.edu.pl>
2024-11-11 10:32:58 +0100

    add zustand

--- src/app/map-screen.tsx
+++ src/app/map-screen.tsx
@@ -7,9 +7,8 @@ import DarkGreenDot from '../../assets/ellipse2.svg';
 import MenuIcon from '../../assets/menus1.svg';
 import NavigationIcon from '../../assets/navigation.svg';
 import SearchIcon1 from '../../assets/search1.svg';
-import SearchIcon2 from '../../assets/search2.svg'; // Icon in top bar
-import BookIcon from '../../assets/book1.svg';
-import BuildingIcon from '../../assets/building3.svg';
+import SearchIcon2 from '../../assets/search2.svg';
+import useLocationStore from '../store/useLocationStore';
 import { useTranslation } from 'react-i18next';
 
 MapLibreGL.setAccessToken(null);
@@ -22,45 +21,24 @@ const campusBounds = {
 
 const campusCenter = [21.04635389581634, 52.16357007158958];
 
-// Locations
-const locations = [
-  { id: 1, name: 'Basen', icon: <BuildingIcon width={24} height={24} /> },
-  { id: 2, name: 'Biblioteka Główna', icon: <BookIcon width={24} height={24} /> },
-  {
-    id: 3,
-    name: 'Biblioteka przy Instytucie Inżynierii',
-    icon: <BookIcon width={24} height={24} />,
-  },
-  {
-    id: 4,
-    name: 'Biblioteka przy Instytucie Medycyny',
-    icon: <BookIcon width={24} height={24} />,
-  },
-  {
-    id: 5,
-    name: 'Biblioteka przy Instytucie Nauk o Zdrowiu',
-    icon: <BookIcon width={24} height={24} />,
-  },
-];
-
-// Type for each location
-interface Location {
-  id: number;
-  name: string;
-  icon: JSX.Element;
-}
-
 export default function MapExample() {
   const { t } = useTranslation();
-  const camera = useRef(null);
+  const camera = useRef<MapLibreGL.CameraRef | null>(null);
   const [isExpanded, setIsExpanded] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filteredLocations, setFilteredLocations] = useState<Location[]>([]);
+  
+  // Zustand store
+  const {
+    searchQuery,
+    filteredLocations,
+    setSearchQuery,
+    filterLocations,
+    clearFilteredLocations,
+  } = useLocationStore();
 
   const expandFullSearchBar = () => {
     setIsExpanded(true);
-    setFilteredLocations([]);
-    setSearchQuery('');
+    clearFilteredLocations();
+    setSearchQuery('');      
   };
 
   const handleMapPress = () => {
@@ -74,23 +52,20 @@ export default function MapExample() {
   const handleSearch = (text: string) => {
     setSearchQuery(text);
     if (text === '') {
-      setFilteredLocations([]);
+      clearFilteredLocations();
     } else {
-      const filtered = locations.filter((location) =>
-        location.name.toLowerCase().includes(text.toLowerCase())
-      );
-      setFilteredLocations(filtered);
+      filterLocations(text);
     }
   };
 
   const handleLocationSelect = (locationName: string) => {
     setSearchQuery(locationName);
     setIsExpanded(false);
-    setFilteredLocations([]);
+    clearFilteredLocations();
   };
 
-  // Render each location item
-  const renderLocationItem = (item: Location) => (
+  // Render location
+  const renderLocationItem = (item: { id: number; name: string; icon: JSX.Element }) => (
     <TouchableOpacity
       key={item.id}
       className="flex-row items-center bg-white p-2"
@@ -129,11 +104,8 @@ export default function MapExample() {
           }`}>
           {isExpanded ? (
             <View className="flex-col">
-              {/* Expanded search */}
               <View className="mb-2 ml-4 flex-row items-center">
-                <View>
-                  <LightGreenDot width={20} height={20} className="ml-4 mr-2" />
-                </View>
+                <LightGreenDot width={20} height={20} className="ml-4 mr-2" />
                 <TextInput
                   className="ml-2 mt-1 flex-1 rounded-md bg-white px-4 text-lg"
                   placeholder={t('map.search.startingPoint')}
@@ -142,9 +114,7 @@ export default function MapExample() {
               </View>
               <View className="my-1 ml-16 h-px w-5/6 bg-gray-300" />
               <View className="ml-4 flex-row items-center">
-                <View>
-                  <DarkGreenDot width={20} height={20} className="ml-4 mr-2" />
-                </View>
+                <DarkGreenDot width={20} height={20} className="ml-4 mr-2" />
                 <TextInput
                   className="ml-2 mt-1 flex-1 rounded-md bg-white px-4 text-lg"
                   placeholder={t('map.search.destination')}
@@ -167,16 +137,15 @@ export default function MapExample() {
           )}
         </View>
 
-        {/* Display filtered locations */}
+        {/* Wyświetlanie filtrowanych lokalizacji */}
         {!isExpanded && filteredLocations.length > 0 && (
           <View
             className="absolute left-4 right-4 z-10 max-h-60 bg-white shadow"
             style={{
-              top: isExpanded ? 140 : 160, // Example pixel values
-              borderBottomLeftRadius: 100, // Rounded bottom-left corner
-              borderBottomRightRadius: 100, // Rounded bottom-right corner
+              top: isExpanded ? 140 : 160,
+              borderBottomLeftRadius: 100,
+              borderBottomRightRadius: 100,
             }}>
-            {/* Separator */}
             <View className="h-px bg-gray-300" />
             {filteredLocations.map((item) => renderLocationItem(item))}
           </View>
