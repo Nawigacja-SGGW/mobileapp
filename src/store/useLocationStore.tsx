e504c9598ce383059b3fc90d63a066320426557a
Kamil Kula <s205833@sggw.edu.pl>
2024-11-12 17:50:23 +0100

    add coordinates and clean code

--- src/store/useLocationStore.tsx
+++ src/store/useLocationStore.tsx
@@ -7,6 +7,7 @@ interface Location {
   id: number;
   name: string;
   icon: JSX.Element;
+  coordinates: [number, number];
 }
 
 interface LocationStore {
@@ -19,26 +20,52 @@ interface LocationStore {
 }
 
 const initialLocations: Location[] = [
-  { id: 1, name: 'Basen', icon: <BuildingIcon width={24} height={24} /> },
-  { id: 2, name: 'Biblioteka Główna', icon: <BookIcon width={24} height={24} /> },
-  { id: 3, name: 'Biblioteka przy Instytucie Inżynierii', icon: <BookIcon width={24} height={24} /> },
-  { id: 4, name: 'Biblioteka przy Instytucie Medycyny', icon: <BookIcon width={24} height={24} /> },
-  { id: 5, name: 'Biblioteka przy Instytucie Nauk o Zdrowiu', icon: <BookIcon width={24} height={24} /> },
+  {
+    id: 1,
+    name: 'Basen',
+    icon: <BuildingIcon width={24} height={24} />,
+    coordinates: [21.0465, 52.1674],
+  },
+  {
+    id: 2,
+    name: 'Biblioteka Główna',
+    icon: <BookIcon width={24} height={24} />,
+    coordinates: [21.0487, 52.1643],
+  },
+  {
+    id: 3,
+    name: 'Biblioteka przy Instytucie Inżynierii',
+    icon: <BookIcon width={24} height={24} />,
+    coordinates: [21.0458, 52.1658],
+  },
+  {
+    id: 4,
+    name: 'Biblioteka przy Instytucie Medycyny',
+    icon: <BookIcon width={24} height={24} />,
+    coordinates: [21.0436, 52.1627],
+  },
+  {
+    id: 5,
+    name: 'Biblioteka przy Instytucie Nauk o Zdrowiu',
+    icon: <BookIcon width={24} height={24} />,
+    coordinates: [21.0419, 52.1612],
+  },
 ];
 
 const useLocationStore = create<LocationStore>((set) => ({
   locations: initialLocations,
   filteredLocations: [],
   searchQuery: '',
-  
+
   setSearchQuery: (query) => set({ searchQuery: query }),
 
-  filterLocations: (query) => set((state) => ({
-    searchQuery: query,
-    filteredLocations: state.locations.filter((location) =>
-      location.name.toLowerCase().includes(query.toLowerCase())
-    ),
-  })),
+  filterLocations: (query) =>
+    set((state) => ({
+      searchQuery: query,
+      filteredLocations: state.locations.filter((location) =>
+        location.name.toLowerCase().includes(query.toLowerCase())
+      ),
+    })),
 
   clearFilteredLocations: () => set({ filteredLocations: [] }),
 }));
