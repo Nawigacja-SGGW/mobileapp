411cc8e04c1bf04d30bdf3ded448876b84b682c4
heisenshark <sylaypaper@gmail.com>
2024-11-12 20:35:40 +0100

    Merge pull request #10 from Nawigacja-SGGW/feature/search-location
    commit de6dfb54e89b16ca7ef35609bad3163f34bc2380
    Merge: 57e1ec3 7cc36a2
    Author: heisenshark <46131664+heisenshark@users.noreply.github.com>
    Date:   Tue Nov 12 20:10:19 2024 +0100
    
        Merge pull request #10 from Nawigacja-SGGW/feature/search-location
    
        add search location feature and 2 icons next to location in filtered search list
    
    commit 7cc36a234ac2b4e942094a3b9ea2a5346009cc43
    Author: heisenshark <sylaypaper@gmail.com>
    Date:   Tue Nov 12 20:07:48 2024 +0100
    
        merge
    
    commit a67801f9233a0cfb93e567e11be432706b60a259
    Merge: 7950b39 dda1d66
    Author: heisenshark <sylaypaper@gmail.com>
    Date:   Tue Nov 12 20:07:16 2024 +0100
    
        merge
    
    commit 7950b39ea4011c27b95787fb75c8edb805e1ced7
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 19:40:55 2024 +0100
    
        merge
    
    commit 38513ba0bb87b35b5245e99bb4e833eb31ecbadd
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 19:18:16 2024 +0100
    
        fix: of a fix of a fix
    
    commit 3bb44b06341b1c8cc564937af9f6d2c103ec1ac2
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 17:50:23 2024 +0100
    
        add coordinates and clean code
    
    commit 985f2e2ba83f8be834426c35580fb11126bd85da
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Mon Nov 11 10:32:58 2024 +0100
    
        add zustand
    
    commit ca2d1cffec0a379c278c77fa98f31237a6cafd5d
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Sun Nov 10 17:10:58 2024 +0100
    
        add search location feature and 2 icons next to location in filtered search list
    
    commit dda1d66202313977fefa6308a2587c216ff1cd55
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 19:40:55 2024 +0100
    
        merge
    
    commit 47ca3040001ab17b23b1e34ea89e2f8065c8484a
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 19:23:50 2024 +0100
    
        Revert "fix"
    
        This reverts commit a9d55e11bdd6cdb3a16ef3dbb0f1d6e091e99fb4.
    
    commit a9d55e11bdd6cdb3a16ef3dbb0f1d6e091e99fb4
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 19:18:16 2024 +0100
    
        fix
    
    commit e504c9598ce383059b3fc90d63a066320426557a
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Tue Nov 12 17:50:23 2024 +0100
    
        add coordinates and clean code
    
    commit 2656580473571cacd5ef9ff5ee5c3fca9e3566a8
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Mon Nov 11 10:32:58 2024 +0100
    
        add zustand
    
    commit 4c0d751a64f3654d67460c7b177685b28c8b56f2
    Merge: a11acc4 57e1ec3
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Sun Nov 10 22:46:52 2024 +0100
    
        Merge branch 'develop' into feature/search-location
    
    commit a11acc41de9c314837a583c7c545f1e0b38f0a19
    Author: Kamil Kula <s205833@sggw.edu.pl>
    Date:   Sun Nov 10 17:10:58 2024 +0100
    
        add search location feature and 2 icons next to location in filtered search list

--- src/app/map-screen.tsx
+++ src/app/map-screen.tsx
@@ -1,14 +1,15 @@
-import MapLibreGL from '@maplibre/maplibre-react-native';
-import { Drawer } from 'expo-router/drawer';
 import React, { useRef, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { View, Text, TextInput } from 'react-native';
-
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Drawer } from 'expo-router/drawer';
+import MapLibreGL from '@maplibre/maplibre-react-native';
 import LightGreenDot from '../../assets/ellipse1.svg';
 import DarkGreenDot from '../../assets/ellipse2.svg';
+import MenuIcon from '../../assets/menus1.svg';
+import NavigationIcon from '../../assets/navigation.svg';
 import SearchIcon1 from '../../assets/search1.svg';
-
 import TopHeader from '~/components/TopHeader';
+import useLocationStore from '../store/useLocationStore';
+import { useTranslation } from 'react-i18next';
 
 MapLibreGL.setAccessToken(null);
 MapLibreGL.setConnected(true);
@@ -22,34 +23,68 @@ const campusCenter = [21.04635389581634, 52.16357007158958];
 
 export default function MapExample() {
   const { t } = useTranslation();
-  const camera = useRef(null);
-  const [isExpanded, setIsExpanded] = useState(true);
+  const camera = useRef<MapLibreGL.CameraRef | null>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleSearchBar = () => {
-    console.log('toggle');
-    setIsExpanded((n) => !n);
+  // Zustand store
+  const {
+    searchQuery,
+    filteredLocations,
+    setSearchQuery,
+    filterLocations,
+    clearFilteredLocations,
+  } = useLocationStore();
+
+  const expandFullSearchBar = () => {
+    setIsExpanded(true);
+    clearFilteredLocations();
+    setSearchQuery('');
   };
 
   const handleMapPress = () => {
-    setIsExpanded(true);
   };
+
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+    if (text === '') {
+      clearFilteredLocations();
+    } else {
+      filterLocations(text);
+    }
+  };
+
+  const handleLocationSelect = (locationName: string) => {
+    setSearchQuery(locationName);
+    setIsExpanded(false);
+    clearFilteredLocations();
+  };
+
+  // Render location
+  const renderLocationItem = (item: { id: number; name: string; icon: JSX.Element }) => (
+    <TouchableOpacity
+      key={item.id}
+      className="flex-row items-center bg-white p-2"
+      onPress={() => handleLocationSelect(item.name)}>
+      <View>{item.icon}</View>
+      <Text className="ml-3 text-lg text-black">{item.name}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <>
       <Drawer.Screen
         options={{
-          header: () => <TopHeader isExpanded={isExpanded} toggleSearchBar={toggleSearchBar} />,
+          header: () => <TopHeader isExpanded={isExpanded} toggleSearchBar={expandFullSearchBar} />,
         }}
       />
-      <Text>Map</Text>
-      <View className="mt-20 flex-1">
+      <View className="flex-1">
         {/* Search Bar */}
         <View
-          className={`absolute left-4 right-4 z-10 mt-6 rounded-[25] bg-white p-3 ${
-            isExpanded ? 'h-32' : 'h-15'
+          className={`absolute left-4 right-4 top-16 z-10 mt-16 rounded-t-3xl bg-white p-3 ${
+            isExpanded ? 'min-h-28 py-4' : 'h-15'
           }`}>
           {isExpanded ? (
-            <>
+            <View className="flex-col">
               <View className="mb-2 ml-4 flex-row items-center">
                 <LightGreenDot width={20} height={20} className="ml-4 mr-2" />
                 <TextInput
@@ -58,7 +93,7 @@ export default function MapExample() {
                   placeholderTextColor="#000"
                 />
               </View>
-              <View className="my-1 ml-16 h-px w-5/6 bg-black" />
+              <View className="my-1 ml-16 h-px w-5/6 bg-gray-300" />
               <View className="ml-4 flex-row items-center">
                 <DarkGreenDot width={20} height={20} className="ml-4 mr-2" />
                 <TextInput
@@ -67,18 +102,35 @@ export default function MapExample() {
                   placeholderTextColor="#000"
                 />
               </View>
-            </>
+            </View>
           ) : (
-            <View className="flex-1 flex-row items-center">
+            <View className="flex-row items-center">
               <SearchIcon1 width={28} height={28} className="mr-2" />
               <TextInput
-                className="ml-3 flex-1 text-lg"
+                className="ml-3 ml-8 flex-1 text-lg"
                 placeholder="Search"
                 placeholderTextColor="#000"
+                value={searchQuery}
+                onChangeText={handleSearch}
+                autoFocus={!isExpanded}
               />
             </View>
           )}
         </View>
+
+        {/* Wyświetlanie filtrowanych lokalizacji */}
+        {!isExpanded && filteredLocations.length > 0 && (
+          <View
+            className="absolute left-4 right-4 z-10 max-h-60 bg-white shadow"
+            style={{
+              top: isExpanded ? 140 : 160,
+              borderBottomLeftRadius: 100,
+              borderBottomRightRadius: 100,
+            }}>
+            <View className="h-px bg-gray-300" />
+            {filteredLocations.map((item) => renderLocationItem(item))}
+          </View>
+        )}
 
         {/* Map View */}
         <MapLibreGL.MapView
