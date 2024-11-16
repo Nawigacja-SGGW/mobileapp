e416b3e063b156363d962f394b891109bc988d4e
mpygruu <barkwa67@gmail.com>
2024-11-17 00:30:46 +0100

    fix register layout

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import Drawer from 'expo-router/drawer';
 import { useNavigation } from 'expo-router';
-import { useTranslation } from 'react-i18next';
+import Drawer from 'expo-router/drawer';
+import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import {StyleSheet, ScrollView} from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet, ScrollView, View } from 'react-native';
 
-import { Logo } from '~/components/Logo';
 import { AppButton } from '~/components/AppButton';
 import { AppInput, AppSecureInput } from '~/components/AppInput';
+import { Logo } from '~/components/Logo';
 
 export default function Register() {
-
   const { control, handleSubmit } = useForm();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isPassword2Visible, setIsPassword2Visible] = useState(false);
@@ -25,9 +24,9 @@ export default function Register() {
     setIsPassword2Visible(!isPassword2Visible);
   };
 
-  const onSubmit = (data:any) => {
+  const onSubmit = (data: any) => {
     console.log(data); // logowanie danych formularza
-    navigation.navigate('map-screen')
+    navigation.navigate('map-screen');
   };
 
   const styles = StyleSheet.create({
@@ -50,70 +49,80 @@ export default function Register() {
       fontWeight: 500,
     },
   });
-  
+
   return (
     <>
-      <Drawer.Screen options={{ headerShown: false, }}/>
-      <ScrollView style={styles.container}>
-        <Logo/>
+      <Drawer.Screen options={{ headerShown: false }} />
 
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange, value } }) => (
-            <AppInput
-            label = {t('login.email.label')}
-            placeholder = {t('login.email.placeholder')}
-            value={value}
-            onChangeText={onChange}
-            keyboardType="email-address" 
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="username"
-          render={({ field: { onChange, value } }) => (
-            <AppInput
-            label = {t('login.username.label')}
-            placeholder = {t('login.username.placeholder')}
-            value={value}
-            onChangeText={onChange}
-            keyboardType="default" 
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="password"
-          render={({ field: { onChange, value } }) => (
-            <AppSecureInput
-            label= {t('login.password.label')}
-            placeholder= {t('login.password.placeholder')}
-            value={value}
-            onChangeText={onChange}
-            keyboardType="default"
-            isPasswordVisible={isPasswordVisible}
-            togglePasswordVisibility={togglePasswordVisibility}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="confirmPassword"
-          render={({ field: { onChange, value } }) => (
-            <AppSecureInput
-            label= {t('login.confirmPassword.label')}
-            placeholder= {t('login.confirmPassword.placeholder')}
-            value={value}
-            onChangeText={onChange}
-            keyboardType="default"
-            isPasswordVisible={isPassword2Visible}
-            togglePasswordVisibility={togglePassword2Visibility}
-            />
-          )}
-        />
-        <AppButton title={t('login.signUpButton')} onPress={handleSubmit(onSubmit)}/>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={{
+          height: '100%',
+          justifyContent: 'space-between',
+          paddingTop: 32,
+          paddingBottom: 32,
+        }}>
+        <Logo />
+        <View>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <AppInput
+                label={t('login.email.label')}
+                placeholder={t('login.email.placeholder')}
+                value={value}
+                onChangeText={onChange}
+                keyboardType="email-address"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="username"
+            render={({ field: { onChange, value } }) => (
+              <AppInput
+                label={t('login.username.label')}
+                placeholder={t('login.username.placeholder')}
+                value={value}
+                onChangeText={onChange}
+                keyboardType="default"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <AppSecureInput
+                label={t('login.password.label')}
+                placeholder={t('login.password.placeholder')}
+                value={value}
+                onChangeText={onChange}
+                keyboardType="default"
+                isPasswordVisible={isPasswordVisible}
+                togglePasswordVisibility={togglePasswordVisibility}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { onChange, value } }) => (
+              <AppSecureInput
+                label={t('login.confirmPassword.label')}
+                placeholder={t('login.confirmPassword.placeholder')}
+                value={value}
+                onChangeText={onChange}
+                keyboardType="default"
+                isPasswordVisible={isPassword2Visible}
+                togglePasswordVisibility={togglePassword2Visibility}
+              />
+            )}
+          />
+        </View>
+
+        <AppButton title={t('login.signUpButton')} onPress={handleSubmit(onSubmit)} />
       </ScrollView>
     </>
   );
