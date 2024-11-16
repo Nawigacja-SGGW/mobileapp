f361fc63c67620b11229e3061dbc328d99689750
CrazyPablo <71659677+CrazyPablo@users.noreply.github.com>
2024-11-16 16:29:53 +0100

    refactor: resetPassword #4

--- src/app/resetPassword.tsx
+++ src/app/resetPassword.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Drawer from 'expo-router/drawer';
 import { useNavigation } from 'expo-router';
 import { useTranslation } from 'react-i18next';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
@@ -15,10 +15,8 @@ export default function ResetPassword() {
   const route = useRoute();
   const { email } = route.params || {};
 
-  const { handleSubmit } = useForm();
-  const [password, setPassword] = useState('');
+  const { control, handleSubmit } = useForm();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const [password2, setPassword2] = useState('');
   const [isPassword2Visible, setIsPassword2Visible] = useState(false);
   const { t } = useTranslation();
   const navigation = useNavigation();
@@ -63,30 +61,42 @@ export default function ResetPassword() {
       <Drawer.Screen options={{ headerShown: false, }}/>
       <ScrollView style={styles.container}>
         <Logo/>
+
         <View style={styles.content}>
           <Text style={styles.title}>{t('login.resetPassword')}</Text>
           <Text style={styles.text}>{t('login.resetPasswordText')+email}</Text>
 
-          <AppSecureInput
-            label= {t('login.newPassword.label')}
-            placeholder= {t('login.newPassword.placeholder')}
-            value={password}
-            onChangeText={setPassword}
-            keyboardType="default"
-            isPasswordVisible={isPasswordVisible}
-            togglePasswordVisibility={togglePasswordVisibility}
-            />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <AppSecureInput
+              label= {t('login.newPassword.label')}
+              placeholder= {t('login.newPassword.placeholder')}
+              value={value}
+              onChangeText={onChange}
+              keyboardType="default"
+              isPasswordVisible={isPasswordVisible}
+              togglePasswordVisibility={togglePasswordVisibility}
+              />
+            )}
+          />
 
-          <AppSecureInput
-            label= {t('login.confirmNewPassword.label')}
-            placeholder= {t('login.confirmNewPassword.placeholder')}
-            value={password2}
-            onChangeText={setPassword2}
-            keyboardType="default"
-            isPasswordVisible={isPassword2Visible}
-            togglePasswordVisibility={togglePassword2Visibility}
-            />
-
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <AppSecureInput
+              label= {t('login.confirmNewPassword.label')}
+              placeholder= {t('login.confirmNewPassword.placeholder')}
+              value={value}
+              onChangeText={onChange}
+              keyboardType="default"
+              isPasswordVisible={isPassword2Visible}
+              togglePasswordVisibility={togglePassword2Visibility}
+              />
+            )}
+          />
           <AppButton title={t('login.resetButton')} onPress={handleSubmit(onSubmit)}/>
         </View>
       </ScrollView>
