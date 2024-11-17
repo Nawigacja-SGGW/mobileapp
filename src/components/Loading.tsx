7a159ee9ca35d71b075f79cdee4a67e77c4debb3
mpygruu <barkwa67@gmail.com>
2024-11-17 19:40:48 +0100

    adjust loading width

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 const Loading = () => {
   return (
-    <View className="absolute left-1/2 top-1/2 z-50 h-12 w-24 flex-1 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-neutral-300 shadow-lg">
+    <View className="absolute left-1/2 top-1/2 z-50 h-12 w-32 flex-1 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-neutral-300 shadow-lg">
       <Text className="text-xl font-medium color-green-main">{t('loading')}...</Text>
     </View>
   );
