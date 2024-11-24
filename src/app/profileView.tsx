307a6d30436703ad06e4051a5a5cbce2c38af6ec
Kamil Kula <s205833@sggw.edu.pl>
2024-11-24 23:43:33 +0100

    format document, implement log out button

--- src/app/profileView.tsx
+++ src/app/profileView.tsx
@@ -3,12 +3,10 @@ import Drawer from 'expo-router/drawer';
 import { View, Text, TouchableOpacity } from 'react-native';
 import ProfilePicture from '../../assets/profile-picture.svg';
 import TopHeader from '~/components/TopHeader';
-
-
+import { router } from 'expo-router';
 
 export default function ProfileView() {
   return (
-
     <View className="flex-1 bg-[#003228] px-6">
       {/* Header */}
       <Drawer.Screen
@@ -18,37 +16,40 @@ export default function ProfileView() {
         }}
       />
       {/* Profile Section */}
-      <View className="items-center mt-52"> 
-          <View className="w-52 h-52 rounded-full"> 
-            <ProfilePicture width="100%" height="100%" />
-          </View>
+      <View className="mt-52 items-center">
+        <View className="h-52 w-52 rounded-full">
+          <ProfilePicture width="100%" height="100%" />
         </View>
+      </View>
 
       {/* User Info */}
       <View className="mt-20 px-4">
         {/* Email */}
-        <View className="w-full max-w-lg mb-14">
-          <Text className="text-[#D3D3D3] text-2xl mb-1">email</Text>
-          <Text className="text-white text-3xl mt-2 font-bold">random@gmail.com</Text>
+        <View className="mb-14 w-full max-w-lg">
+          <Text className="mb-1 text-2xl text-[#D3D3D3]">email</Text>
+          <Text className="mt-2 text-3xl font-bold text-white">random@gmail.com</Text>
         </View>
 
         {/* My Kilometers */}
-        <View className="w-full max-w-lg mb-14">
-          <Text className="text-[#D3D3D3] text-2xl mb-1">my kilometers</Text>
-          <Text className="text-white text-3xl mt-2 font-bold">1,2</Text>
+        <View className="mb-14 w-full max-w-lg">
+          <Text className="mb-1 text-2xl text-[#D3D3D3]">my kilometers</Text>
+          <Text className="mt-2 text-3xl font-bold text-white">1,2</Text>
         </View>
 
         {/* My Routes */}
-        <View className="w-full max-w-lg mb-8">
-          <Text className="text-[#D3D3D3] text-2xl mb-1">my routes</Text>
-          <Text className="text-white text-3xl mt-2 font-bold">5</Text>
+        <View className="mb-8 w-full max-w-lg">
+          <Text className="mb-1 text-2xl text-[#D3D3D3]">my routes</Text>
+          <Text className="mt-2 text-3xl font-bold text-white">5</Text>
         </View>
       </View>
 
       {/* Logout Button */}
       <View className="absolute bottom-6 left-6 right-6">
-        <TouchableOpacity className="bg-white py-4 rounded-full items-center w-3/4 mx-auto">
-          <Text className="text-black font-extrabold text-xl">log out</Text>
+        <TouchableOpacity
+          className="mx-auto w-3/4 items-center rounded-full bg-white py-4"
+          onPress={() => router.push('/start')} // Funkcja do przejścia do widoku start.tsx
+        >
+          <Text className="text-xl font-extrabold text-black">log out</Text>
         </TouchableOpacity>
       </View>
     </View>
