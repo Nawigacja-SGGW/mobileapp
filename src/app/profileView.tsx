3b76fa86f5e1a53197cd2cb71a49120387e1038d
Kamil Kula <s205833@sggw.edu.pl>
2024-11-25 16:52:54 +0100

    translacje

--- src/app/profileView.tsx
+++ src/app/profileView.tsx
@@ -4,8 +4,10 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import ProfilePicture from '../../assets/profile-picture.svg';
 import TopHeader from '~/components/TopHeader';
 import { router } from 'expo-router';
+import { useTranslation } from 'react-i18next';
 
 export default function ProfileView() {
+  const { t } = useTranslation();
   return (
     <View className="flex-1 bg-[#003228] px-6">
       {/* Header */}
@@ -26,19 +28,19 @@ export default function ProfileView() {
       <View className="mt-20 px-4">
         {/* Email */}
         <View className="mb-14 w-full max-w-lg">
-          <Text className="mb-1 text-2xl text-[#D3D3D3]">email</Text>
+          <Text className="mb-1 text-2xl text-[#D3D3D3]">{t('profile.email')}</Text>
           <Text className="mt-2 text-3xl font-bold text-white">random@gmail.com</Text>
         </View>
 
         {/* My Kilometers */}
         <View className="mb-14 w-full max-w-lg">
-          <Text className="mb-1 text-2xl text-[#D3D3D3]">my kilometers</Text>
+          <Text className="mb-1 text-2xl text-[#D3D3D3]">{t('profile.myKilometers')}</Text>
           <Text className="mt-2 text-3xl font-bold text-white">1,2</Text>
         </View>
 
         {/* My Routes */}
         <View className="mb-8 w-full max-w-lg">
-          <Text className="mb-1 text-2xl text-[#D3D3D3]">my routes</Text>
+          <Text className="mb-1 text-2xl text-[#D3D3D3]">{t('profile.myRoutes')}</Text>
           <Text className="mt-2 text-3xl font-bold text-white">5</Text>
         </View>
       </View>
@@ -48,7 +50,7 @@ export default function ProfileView() {
         <TouchableOpacity
           className="mx-auto w-3/4 items-center rounded-full bg-white py-4"
           onPress={() => router.push('/start')}>
-          <Text className="text-xl font-extrabold text-black">log out</Text>
+          <Text className="text-xl font-extrabold text-black">{t('profile.logOut')}</Text>
         </TouchableOpacity>
       </View>
     </View>
