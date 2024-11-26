37f8bb3bb76fb3c1bfe21e6a3df689e5130e96aa
Kamil Kula <s205833@sggw.edu.pl>
2024-11-26 20:16:25 +0100

    fix responsiveness 😎

--- src/app/profileView.tsx
+++ src/app/profileView.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Drawer from 'expo-router/drawer';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import ProfilePicture from '../../assets/profile-picture.svg';
 import TopHeader from '~/components/TopHeader';
 import { useRouter } from 'expo-router';
 import { useTranslation } from 'react-i18next';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const { height } = Dimensions.get('window');
 
 export default function ProfileView() {
   const router = useRouter();
   const { t } = useTranslation();
 
   const handleLogout = () => {
-    console.log("Wylogowanie");
-    router.replace('/start');
+    router.push('/start');
   };
+
   return (
-    <View className="flex-1 bg-[#003228] px-6">
+    <SafeAreaView className="flex-1 bg-[#003228] px-4">
       {/* Header */}
       <Drawer.Screen
         options={{
@@ -23,31 +26,32 @@ export default function ProfileView() {
           header: () => <TopHeader onlyBack={true} modeSearch={''} toggleSearchBar={() => {}} />,
         }}
       />
+
       {/* Profile Section */}
-      <View className="mt-52 items-center">
+      <View className="items-center" style={{ marginTop: height * 0.17 }}>
         <View className="h-52 w-52 rounded-full">
           <ProfilePicture width="100%" height="100%" />
         </View>
       </View>
 
       {/* User Info */}
-      <View className="mt-20 px-4">
+      <View className="mt-10 px-4">
         {/* Email */}
-        <View className="mb-14 w-full max-w-lg">
-          <Text className="mb-1 text-2xl text-[#D3D3D3]">{t('profile.email')}</Text>
-          <Text className="mt-2 text-3xl font-bold text-white">random@gmail.com</Text>
+        <View className="mb-8 w-full max-w-lg">
+          <Text className="mb-1 text-lg text-[#D3D3D3]">{t('profile.email')}</Text>
+          <Text className="mt-2 text-2xl font-bold text-white">random@gmail.com</Text>
         </View>
 
         {/* My Kilometers */}
-        <View className="mb-14 w-full max-w-lg">
-          <Text className="mb-1 text-2xl text-[#D3D3D3]">{t('profile.myKilometers')}</Text>
-          <Text className="mt-2 text-3xl font-bold text-white">1,2</Text>
+        <View className="mb-8 w-full max-w-lg">
+          <Text className="mb-1 text-lg text-[#D3D3D3]">{t('profile.myKilometers')}</Text>
+          <Text className="mt-2 text-2xl font-bold text-white">1,2</Text>
         </View>
 
         {/* My Routes */}
         <View className="mb-8 w-full max-w-lg">
-          <Text className="mb-1 text-2xl text-[#D3D3D3]">{t('profile.myRoutes')}</Text>
-          <Text className="mt-2 text-3xl font-bold text-white">5</Text>
+          <Text className="mb-1 text-lg text-[#D3D3D3]">{t('profile.myRoutes')}</Text>
+          <Text className="mt-2 text-2xl font-bold text-white">5</Text>
         </View>
       </View>
 
@@ -59,6 +63,6 @@ export default function ProfileView() {
           <Text className="text-xl font-extrabold text-black">{t('profile.logOut')}</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
