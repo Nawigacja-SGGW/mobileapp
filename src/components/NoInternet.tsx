cadd0a424a7435761937bf0917f443e1b5171647
mpygruu <barkwa67@gmail.com>
2025-01-14 12:23:59 +0100

    add translations + desc ui adjustment

--- src/components/NoInternet.tsx
+++ src/components/NoInternet.tsx
@@ -9,13 +9,11 @@ export const NoInternet = () => {
   return (
     <View className="absolute z-50 h-full w-screen flex-1 items-center justify-center bg-green-main">
       <Image source={require('./../../assets/no-internet.png')} />
-      <Text className="pb-2 text-2xl font-bold text-white">Brak internetu!</Text>
-      <Text className="text-xl font-medium text-white">
-        Aplikacja wymaga stałego dostępu do internetu.
-      </Text>
+      <Text className="pb-2 text-2xl font-bold text-white">{t('noInternet.title')}</Text>
+      <Text className="text-xl text-neutral-200">{t('noInternet.text')}</Text>
       <TouchableOpacity className="absolute bottom-12 rounded-full bg-white p-4">
-        <Text className="text-xloce h-8 w-64 text-center font-bold text-black">
-          Spróbuj ponownie
+        <Text className="h-8 w-64 text-center text-xl font-bold text-black">
+          {t('noInternet.tryAgain')}
         </Text>
       </TouchableOpacity>
     </View>
