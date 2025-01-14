e43a111bc8d235beeda9f952e7136a519741e348
mpygruu <barkwa67@gmail.com>
2025-01-14 20:00:47 +0100

    remove unused imports

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,6 @@
-import { useTranslation } from 'react-i18next';
 import { View, ActivityIndicator } from 'react-native';
 
 const Loading = () => {
-  const { t } = useTranslation();
-
   return (
     <View className="absolute left-1/2 top-1/2 z-50 h-16 w-16 flex-1 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-neutral-100 shadow-lg">
       <ActivityIndicator size="large" color="#003228" />
